ab082216782412ab0618c756010a366678ee6943
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-04-06 21:37:37 +0530

    Replace data array in test function with detailed user information

--- scriplt.js
+++ scriplt.js
@@ -1,7 +1,15 @@
 function test() {
 
+    let data = [
 
-    let data = [ 10, 20, 30, 40 ];
+        [1, "Chinthaka", "Moneragala"],
+        [2, "Nimal", "Colombo"],
+        [3, "Kumari", "Kandy"]
+
+    ];
+
+
+
     console.log(data);
 
 }
