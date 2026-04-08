7d28d326fd13413189b2bf4782de73ed9f1dca1a
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-04-08 19:47:38 +0530

    Refactor test function to use numerical data for training and update weight calculations

--- scriplt.js
+++ scriplt.js
@@ -2,15 +2,37 @@ function test() {
 
     let data = [
 
-        [1, "Chinthaka", "Moneragala"],
-        [2, "Kasun", "Kandy"],
+        [1, 1, 1],
+        [0, 0, 0],
+        [1, 0, 1],
+        [0, 1, 0]
 
     ];
 
-    for (let x = 0; x < data.length; x++) {
+    let weaight1 = 0.6;
+    let weaight2 = 0.2;
+    let bias = -2;
+    let learningRate = 0.5;
 
-        console.log(data[x][1]);
+    for (let i = 0; i < 10000; i++) {
 
+        for (let j = 0; j < data.length; j++) {
+
+            let input1 = data[j][0];
+            let input2 = data[j][1];
+            let expectedoutput = data[j][2];
+
+            let weightedSum = (input1 * weaight1) + (input2 * weaight2) + bias;
+            let activation = 1/(1 + Math.exp(-weightedSum));
+
+            let error = expectedoutput - activation;
+
+            weaight1 = weaight1 + (learningRate * error * input1);
+            weaight2 = weaight2 + (learningRate * error * input2);
+            bias = bias + (learningRate * error);
+
+            console.log(weaight1 + ", " + weaight2 + ", " + bias);
+
+        }
     }
-
 }
